41af0d4fd19f7164da48f6fcde3c98fa6966baca
Tayebwa Noah <ndacyayinoah@gmail.com>
2025-01-21 22:06:15 +0300

    Update views.py

--- app_X/views.py
+++ app_X/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Profile, Post, Comment
 from django.contrib import messages
+from .models import Profile, Post, Comment
 
-# Create your views here.
+# Helper Functions
+def get_user_profile(request):
+    """Helper function to get the user profile from the session."""
+    return get_object_or_404(Profile, id=request.session['user'])
+
+def get_common_context(request):
+    """Helper function to get common context data for views."""
+    user_profile = get_user_profile(request)
+    return {
+        'User': user_profile,
+        'Following': user_profile.following.all(),
+        'Followers': user_profile.followers.all(),
+        'status': request.session.get('word', '')
+    }
+
+def redirect_if_no_user(request):
+    """Helper function to redirect if no user is logged in."""
+    if 'user' not in request.session:
+        return redirect('no_user')
+    return None
 
 # WISE WELCOME_________
 def xplanet(request):
     context = {}
     if 'user' in request.session:
-        context['User'] = get_object_or_404(Profile, id=request.session['user'])
+        context['User'] = get_user_profile(request)
         context['status'] = request.session.get('word', '')
     else:
         context['No_user'] = "Not Signed In!"
@@ -19,34 +38,25 @@ def no_user(request):
 
 # DASHBOARD PAGE FUNCTIONS________
 def dashboard(request):
-    if 'user' not in request.session:
+    if redirect_if_no_user(request):
         return redirect('no_user')
 
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
-    context = {
-        'User': user_profile,
+    user_profile = get_user_profile(request)
+    context = get_common_context(request)
+    context.update({
         'User_liked': user_profile.posts_liked.all().order_by("-id"),
         'User_comments': user_profile.profile_comments.all().order_by("-id"),
         'Posts': user_profile.posts.all().order_by("-id"),
-        'Following': user_profile.following.all(),
-        'Followers': user_profile.followers.all(),
-        'status': request.session.get('word', '')
-    }
+    })
     return render(request, 'dashboard.html', context)
 
 # FORUM PAGE FUNCTIONS _________
 def forum(request):
-    if 'user' not in request.session:
+    if redirect_if_no_user(request):
         return redirect('no_user')
 
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
-    context = {
-        'User': user_profile,
-        'status': request.session.get('word', ''),
-        'Posts': Post.objects.all().order_by('-id'),
-        'Following': user_profile.following.all(),
-        'Followers': user_profile.followers.all(),
-    }
+    context = get_common_context(request)
+    context['Posts'] = Post.objects.all().order_by('-id')
     return render(request, 'forum.html', context)
 
 def post(request):
@@ -60,22 +70,16 @@ def post(request):
     Post.objects.create(
         title=request.POST['title'],
         post=request.POST['post'],
-        profile=get_object_or_404(Profile, id=request.session['user'])
+        profile=get_user_profile(request)
     )
     return redirect('forum')
 
 def viewpost(request, num):
-    if 'user' not in request.session:
+    if redirect_if_no_user(request):
         return redirect('no_user')
 
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
-    context = {
-        'User': user_profile,
-        'status': request.session.get('word', ''),
-        'Post': get_object_or_404(Post, id=num),
-        'Following': user_profile.following.all(),
-        'Followers': user_profile.followers.all(),
-    }
+    context = get_common_context(request)
+    context['Post'] = get_object_or_404(Post, id=num)
     return render(request, 'postview.html', context)
 
 def editpost(request):
@@ -102,7 +106,7 @@ def comment(request):
 
     Comment.objects.create(
         post=get_object_or_404(Post, id=request.POST['comkey']),
-        profile=get_object_or_404(Profile, id=request.session['user']),
+        profile=get_user_profile(request),
         comment=request.POST['comment']
     )
     return redirect('forum')
@@ -114,33 +118,27 @@ def delete(request):
 
 # LIKES/DISLIKES_____________________
 def like(request):
-    user = get_object_or_404(Profile, id=request.session['user'])
+    user = get_user_profile(request)
     post = get_object_or_404(Post, id=request.POST['jugkey'])
     post.liked.add(user)
     return redirect('forum')
 
 def dislike(request):
-    user = get_object_or_404(Profile, id=request.session['user'])
+    user = get_user_profile(request)
     post = get_object_or_404(Post, id=request.POST['jugkey'])
     post.liked.remove(user)
     return redirect('forum')
 
 # Settings Page________________
 def settings(request):
-    if 'user' not in request.session:
+    if redirect_if_no_user(request):
         return redirect('no_user')
 
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
-    context = {
-        'User': user_profile,
-        'Following': user_profile.following.all(),
-        'Followers': user_profile.followers.all(),
-        'status': request.session.get('word', '')
-    }
+    context = get_common_context(request)
     return render(request, 'settings.html', context)
 
 def update(request):
-    profile = get_object_or_404(Profile, id=request.session['user'])
+    profile = get_user_profile(request)
     errors = Profile.objects.valid_pro(request.POST, request.session)
     if errors:
         for val in errors.values():
@@ -158,28 +156,22 @@ def update(request):
 
 # Humans Page
 def humans(request):
-    if 'user' not in request.session:
+    if redirect_if_no_user(request):
         return redirect('no_user')
 
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
-    context = {
-        'User': user_profile,
-        'Following': user_profile.following.all(),
-        'Followers': user_profile.followers.all(),
-        'status': request.session.get('word', ''),
-        'All': Profile.objects.all().order_by('name')
-    }
+    context = get_common_context(request)
+    context['All'] = Profile.objects.all().order_by('name')
     return render(request, 'humans.html', context)
 
 def follow(request, num):
-    me = get_object_or_404(Profile, id=request.session['user'])
+    me = get_user_profile(request)
     you = get_object_or_404(Profile, id=num)
     you.followers.add(me)
     me.following.add(you)
     return redirect('humans')
 
 def unfollow(request, num):
-    me = get_object_or_404(Profile, id=request.session['user'])
+    me = get_user_profile(request)
     you = get_object_or_404(Profile, id=num)
     you.followers.remove(me)
     me.following.remove(you)
@@ -187,31 +179,21 @@ def unfollow(request, num):
 
 # View Human
 def viewhuman(request, name):
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
+    context = get_common_context(request)
     them_profile = get_object_or_404(Profile, name=name)
-    context = {
-        'User': user_profile,
+    context.update({
         'Them': them_profile,
         'Them_liked': them_profile.posts_liked.all().order_by("-id"),
         'Them_comments': them_profile.profile_comments.all().order_by("-id"),
         'Posts': them_profile.posts.all().order_by("-id"),
-        'Following': them_profile.following.all(),
-        'Followers': them_profile.followers.all(),
-        'status': request.session.get('word', '')
-    }
+    })
     return render(request, 'viewhuman.html', context)
 
 # News Feed Page_____________
 def news(request):
-    if 'user' not in request.session:
+    if redirect_if_no_user(request):
         return redirect('no_user')
 
-    user_profile = get_object_or_404(Profile, id=request.session['user'])
-    context = {
-        'User': user_profile,
-        'status': request.session.get('word', ''),
-        'Posts': Post.objects.all().order_by('-id'),
-        'Following': user_profile.following.all(),
-        'Followers': user_profile.followers.all(),
-    }
+    context = get_common_context(request)
+    context['Posts'] = Post.objects.all().order_by('-id')
     return render(request, 'newsfeed.html', context)
